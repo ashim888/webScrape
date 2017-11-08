fe6f6a52e4f235fcfb53b45fb8d2f685a334dc6b
ashim888 <punchedrock@gmail.com>
2017-11-08 22:57:02 +0545

    fixed code using request lib and one scrape DRY

--- beauti.py
+++ beauti.py
@@ -1,6 +1,7 @@
 import bs4 as bs
-import urllib.request
+# import urllib.request
 import re
+import requests
 
 url = ['https://techcrunch.com/', 'https://thenextweb.com/latest/', 'http://www.foxnews.com/']
 
@@ -9,11 +10,20 @@ all_links =[]
 all_img_links = []
 all_description = []
 
+agents = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
+
+def scrape(url):
+	getPageData = requests.get(url,headers=agents)
+	return bs.BeautifulSoup(getPageData.text, 'lxml')
+
 def get_link(url):
 	i = 0
 
-	getPageData = urllib.request.urlopen(url).read()
-	soup = bs.BeautifulSoup(getPageData, 'lxml')
+	# getPageData = urllib.request.urlopen(url).read()
+	# soup = bs.BeautifulSoup(getPageData, 'lxml')
+	soup = scrape(url)
 
 	# From techcrunch
 	my_titles = soup.findAll("h2", { "class" : "post-title" })
@@ -32,15 +42,18 @@ def get_link(url):
 			break		
 
 def get_data(url):
-	description = ''
-	getPageData = urllib.request.urlopen(url).read()
-	soup = bs.BeautifulSoup(getPageData, 'lxml')
+	# description = ''
+	# getPageData = urllib.request.urlopen(url).read()
+	# soup = bs.BeautifulSoup(getPageData, 'lxml')
+	soup = scrape(url)
 	
 	# getting article titles
-	my_title = soup.findAll("h1", {"class", "tweet-title"})
-	for title in my_title:
-		strTitle = title.string
-		all_title.append(strTitle)
+	title = soup.find("div",class_="l-main").find('h1').get_text()
+	print title
+	# my_title = soup.findAll("h1", {"class", "tweet-title"})
+	# for title in my_title:
+	# 	strTitle = title.string
+	# 	all_title.append(strTitle)
 	
 		# Testing
 		# print (strTitle)
@@ -62,24 +75,28 @@ def get_data(url):
 	# getting article description
 	
 	my_desc = soup.find("div", {"class", "article-entry"}).findAll('p')
-	for desc in my_desc:
-		new_desc = desc.text
-		description = description+'\n'+new_desc
+	for x in soup.find_all("div",class_="article-entry"):
+		body= [y.get_text() for y in x.find_all("p")]
+		print body
+	# for desc in my_desc:
+	# 	new_desc = desc.text
+	# 	description = description+'\n'+new_desc
 		
 		# Testing
 		# print(new_desc,"\n")
 		# all_description.append(desc.text);
 
-	all_description.append(description)	
+	# all_description.append(description)	
 
 # Calling functions
 get_link(url[0])
-for i in range(len(all_links)):
-	get_data(all_links[i])
+# for i in range(len(all_links)):
+	# get_data(all_links[i])
+get_data(all_links[0])
 
 
 print('links: ', all_links,"\n \n")
-print('title: ', all_title,"\n \n")
-for d in all_description:
-	print('Desc: ', d,"\n \n")
+# print('title: ', all_title,"\n \n")
+# for d in all_description:
+# 	print('Desc: ', d,"\n \n")
 print('imglinks: ', all_img_links)
